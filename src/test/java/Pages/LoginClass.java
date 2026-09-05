1cbb40c33e489f4c551e1c7babb8500c8ab0bdd3
Tmukhanu <abumukhanu@gmail.com>
2026-09-05 20:31:11 +0200

    created methods on the login calss

--- src/test/java/Pages/LoginClass.java
+++ src/test/java/Pages/LoginClass.java
@@ -1,4 +1,53 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class LoginClass {
-}
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void setup() throws InterruptedException {
+        driver = new ChromeDriver();
+        Thread.sleep(2000);
+        driver.get("https://ndosisimplifiedautomation.vercel.app/#dashboard");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void ClickLoginButton() throws InterruptedException {
+        driver.findElement(By.xpath("//*[@id=\"app-root\"]/nav/div[1]/div[3]/button")).click();
+    }
+
+    @Test(dependsOnMethods = {"ClickLoginButton"})
+    public void EnterUserName() throws InterruptedException {
+        driver.findElement(By.id("login-email")).sendKeys("mukhanu2@gmail.com");
+    }
+
+    @Test(dependsOnMethods = {"EnterUserName"})
+    public void EnterPassword() throws InterruptedException {
+        driver.findElement(By.id("login-password")).sendKeys("@123456789");
+    }
+
+    @Test(dependsOnMethods = {"EnterPassword"})
+    public void ClickLoginSubmitButton() throws InterruptedException {
+        driver.findElement(By.id("login-submit")).click();
+        Thread.sleep(2000);
+    }
+
+    @Test(dependsOnMethods = {"ClickLoginSubmitButton"})
+    public void VerifyLoginSuccess() throws InterruptedException {
+        driver.findElement(By.xpath("//*[@id=\"app-root\"]/nav/div[1]/div[2]/div[3]/button/span[2]")).isDisplayed();
+        }
+
+        @AfterTest
+        public void quit(){
+            driver.quit();
+        }
+    }
+
