1040d68dc5cb4b15f215453b1e11b883d76e0be3
Tmukhanu <abumukhanu@gmail.com>
2026-09-06 19:15:38 +0200

    Added Browser Factory Class

--- src/test/java/Utils/BrowserFactory.java
+++ src/test/java/Utils/BrowserFactory.java
@@ -1,4 +1,24 @@
 package Utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class BrowserFactory {
+    static WebDriver driver;
+
+    public static WebDriver launchBrowser(String browserChoice, String url) {
+        if (browserChoice.equalsIgnoreCase("chrome")){
+            ChromeOptions options = new ChromeOptions();
+            driver = new ChromeDriver(options);
+    }else if (browserChoice.equalsIgnoreCase("edge")){
+            driver = new EdgeDriver();
+        }else{
+            driver = new FirefoxDriver();
+        }
+        driver.manage().window().maximize();
+        driver.get(url);
+        return driver;
 }
