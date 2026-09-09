3e6c685a511bd1dd86a5df10213555d9bcbfb3f0
Tmukhanu <abumukhanu@gmail.com>
2026-09-09 20:41:34 +0200

    created Login method on the loginPage class

--- src/test/java/Tests/Base.java
+++ src/test/java/Tests/Base.java
@@ -1,6 +1,7 @@
 package Tests;
 
 import Pages.HomePage;
+import Pages.LoginPage;
 import Utils.BrowserFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
@@ -10,5 +11,6 @@ public class Base {
 
     final WebDriver driver = browserFactory.launchBrowser("chrome", "https://ndosisimplifiedautomation.vercel.app/#dashboard");
     public HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+    public LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
 }
 
--- src/test/java/Tests/LoginTest.java
+++ src/test/java/Tests/LoginTest.java
@@ -4,8 +4,13 @@ import Pages.HomePage;
 import org.testng.annotations.Test;
 
 public class LoginTest extends Base {
+
     @Test
-    public void LoginClass() {
+    public void ClickLoginButton() {
         homePage.clickLoginButton();
+    }
+
+    @Test
+    public void EnterUserName() {
     }
 }
